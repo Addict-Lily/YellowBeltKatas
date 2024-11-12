83ece0281c786caeec4e094f118674853b8ec9c5
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-12 21:20:40 +0100

    In Progres

--- MiniKata5_SimpleMethods/Kata5_MethodsAndParameters/Program.cs
+++ MiniKata5_SimpleMethods/Kata5_MethodsAndParameters/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string enemyName = "Goblin";
+        int damage = 20;
+        AttackEnemy(enemyName, damage);
+
+        string playerName = "Arin";
+        int healAmount = 15;
+        HealPlayer(playerName, healAmount);
+    }
+    static void AttackEnemy(string enemyName, int damage)
+    {
+        Console.WriteLine($"Attacked {enemyName} and dealt {damage}! ");
+    }
+
+    static void HealPlayer(string playerName, int healAmount)
+    {
+        Console.WriteLine($"Player {playerName} healed {healAmount} health points!");
     }
 }
