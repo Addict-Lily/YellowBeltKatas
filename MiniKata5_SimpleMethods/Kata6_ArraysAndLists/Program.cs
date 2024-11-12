2acf43bfb537f88d8d23c8fc9e0ace423aec0ca6
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-12 23:42:52 +0100

    In Progress

---
+++ MiniKata5_SimpleMethods/Kata6_ArraysAndLists/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Kata6_ArraysAndLists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enemies: ");
+        string[] enemyNames = new string[5];
+
+        enemyNames[0] = "Goblin";
+        enemyNames[1] = "Orc";
+        enemyNames[2] = "Troll";
+        enemyNames[3] = "Skeleton";
+        enemyNames[4] = "Dragon";
+
+        foreach (string enemyName in enemyNames)
+        {
+            Console.WriteLine(enemyName);
+        }
+        
+        
+        Console.WriteLine("\nPlayer Inventory: ");
+        List<string> playerInventory = new List<string> {"Sword", "Shield", "Potion"};
+
+        foreach (string playerInventoryItem in playerInventory)
+        {
+            Console.WriteLine(playerInventoryItem);
+        }
+        
+        
+        Console.WriteLine("\nUpdated Inventory: ");
+        
+        playerInventory.Add("Helmet");
+        playerInventory.Add("Armor");
+        playerInventory.Remove("Potion");
+        
+        foreach (string playerInventoryItem in playerInventory)
+        {
+            Console.WriteLine(playerInventoryItem);
+        }
+        
+        Console.WriteLine($"\nTotal Items in Inventory: {playerInventory.Count}");
+        
+    }
+    
+}
