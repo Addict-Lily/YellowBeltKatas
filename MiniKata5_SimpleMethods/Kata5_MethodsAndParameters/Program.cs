fb36183f633cb08d733c5a038eab1edfada608ee
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-12 21:04:08 +0100

    In Progress

---
+++ MiniKata5_SimpleMethods/Kata5_MethodsAndParameters/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Kata5_MethodsAndParameters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ MiniKata5_SimpleMethods/MiniKata5_SimpleMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniKata5_SimpleMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int damage = 15;
+        Attack(damage);
+
+        int healAmount = 10;
+        Heal(healAmount);
+    }
+    static void Attack(int damage)
+    {
+        Console.WriteLine($"Player dealt {damage}!");
+    }
+    static void Heal(int healAmount)
+    {
+        Console.WriteLine($"Player healed {healAmount} health points!");
+    }
+}
