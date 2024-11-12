53194a9754a877638a76b2506b8470dc058dccea
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-12 22:55:53 +0100

    In Progress

---
+++ MiniKata5_SimpleMethods/MiniKata6_ArraysAndListsPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MiniKata6_ArraysAndListsPractice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enemies: ");
+        
+        string[] enemyNames = { "Goblin", "Orc", "Troll"};
+
+        foreach (string enemyName in enemyNames)
+        {
+            Console.WriteLine(enemyName);
+        }
+        
+        Console.WriteLine("\nPlayer inventory: ");
+
+        List<string> playerInventory = new List<string> {"Sword", "Shield", "Potion"};
+        
+        foreach (string playersInventories in playerInventory)
+        {
+            Console.WriteLine(playersInventories);
+        }
+        
+        Console.WriteLine("\nUpdated Inventory: ");
+
+        playerInventory.Add("Helmet");
+        
+        foreach (string playersInventories in playerInventory)
+        {
+            Console.WriteLine(playersInventories);
+        }
+        
+    }
+}
