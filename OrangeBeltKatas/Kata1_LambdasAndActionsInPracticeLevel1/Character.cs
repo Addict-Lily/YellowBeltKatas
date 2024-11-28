a118acce282b3bb1c9c125f55292b87783743804
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-28 14:06:34 +0100

    Done

---
+++ OrangeBeltKatas/Kata1_LambdasAndActionsInPracticeLevel1/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata1_Lambdas;
+public class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public Action PrimaryAction { get; set; }
+    public Character(string name, int health, Action primaryAction)
+    {
+        Name = name;
+        Health = health;
+        PrimaryAction = primaryAction;
+    }
+    public void PerformAction()
+    {
+        PrimaryAction.Invoke();
+    }
+}
---
+++ OrangeBeltKatas/Kata1_LambdasAndActionsInPracticeLevel1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Kata1_Lambdas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var warrior = new Character("Warrior",30,() => Console.WriteLine("Warrior attacks with a sword!"));
+        var healer = new Character("Healer",60,() => Console.WriteLine("Healer heals!"));
+        
+        List<Character> characters = new List<Character> { warrior, healer };
+
+
+        foreach (var character in characters)
+        {
+            if (character.Health < 50)
+            {
+                character.PerformAction();
+            }
+        } 
+        if(healer.Health >= 50)
+        {
+            var characterToHeal = characters.Where(c => c.Health < 50).OrderBy(c => c.Health).FirstOrDefault();
+            if (characterToHeal != null)
+            {
+                Console.WriteLine($"{healer.Name} heals {characterToHeal.Name}.");
+                characterToHeal.Health += 20;
+                healer.PerformAction();
+            }
+            else
+            {
+                Console.WriteLine("Healer has no one to heal!");
+            }
+        }
+        
+        foreach (var character in characters.Where(c => c.Health < 50))
+        {
+            character.Health += 10;
+            character.PerformAction();
+        }
+            
+    }
+}
